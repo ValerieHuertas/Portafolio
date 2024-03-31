819f66c82b59157f3d26c7310fa8be42967680ca
ValerieHuertas <137791653+ValerieHuertas@users.noreply.github.com>
2024-03-31 00:49:41 -0500

    Add files via upload
    commit two by david whats button & dark-theme

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function interfaz() {
+    const hojaEstilo = document.getElementById("hoja-estilo");
+
+    if (hojaEstilo.getAttribute("href") === "css/style.css") {
+        hojaEstilo.setAttribute("href", "css/styledark.css");
+    } else {
+        hojaEstilo.setAttribute("href", "css/style.css");
+    }
+
+    const btnInterfaz = document.getElementById("btn-interfaz");
+    
+    if (btnInterfaz.getAttribute("src") === "img/sol.png") {
+        btnInterfaz.setAttribute("src", "img/luna.png");
+    } else {
+        btnInterfaz.setAttribute("src", "img/sol.png");
+    }
+}
